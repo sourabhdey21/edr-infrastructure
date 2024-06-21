4d03980eef173ae1c397dc88e975032f7c50b9ba
root <sourabhdey21@gmail.com>
2024-06-21 20:09:52 +0000

    comment main

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-#Create a bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-nam9u8r9"
-  acl    = "private"
-}
+# #Create a bucket
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "my-unique-bucket-nam9u8r9"
+#   acl    = "private"
+# }
 
 
 
