b01f779badc77ac2fd00eb763db8653d715dc94f
root <sourabhdey21@gmail.com>
2024-06-21 20:02:44 +0000

    main tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-nam9u8r9"
   acl    = "private"
 }
+
+
+resource "aws_eip" "my_eip" {
+  vpc = true
+}
+
+output "elastic_ip" {
+  value = aws_eip.my_eip.public_ip
+}
